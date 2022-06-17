77a770db1167ead889a4f28eb9bb26f561be675b
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2022-06-17 11:17:54 -0300

    Created first test

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # electronic-sales-system-backend
 Electronic sales system aimed at stores
+
+# Models
+
+1. Clients
+1. Products
+1. Payments
---
+++ src/tests/client/create-client.spec.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+it('should successfully create a new client', async () => {
+
+    const response = await axios({
+        url: 'http://localhost:8080/clients/',
+        method: 'POST',
+        data: {
+            name: 'Client name',
+            phoneNumber: '+00 (00) 0 0000-0000',
+            cpf: '000.000.000-00',
+            address: 'Client address street 134, b'
+        }
+    });
+
+    expect(response.status).toBe(200);
+
+});
